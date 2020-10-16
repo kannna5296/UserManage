705182f46dca4ebd001b6e2b6a0f9df4e5061b8c
kannna5296 <kannnahacks5296@gmail.com>
2020-10-16 09:37:37 +0900

    local開発修正

--- docker/mysql/sample.sql
+++ docker/mysql/sample.sql
@@ -3,7 +3,7 @@ create table sample.users(
   id bigint(20) unsigned AUTO_INCREMENT PRIMARY KEY,
   name varchar(100),
   age tinyint,
-  created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  updated_date DATETIME
+  created_date timestamp not null default current_timestamp,
+  updated_date timestamp not null default current_timestamp on update current_timestamp
 );
 INSERT INTO sample.users(name,age) VALUES ('yamane',30),('yamane2',25),('yamane3',19);
